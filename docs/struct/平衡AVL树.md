92bc0f0570f1a693ff385e8940d05cf1b6c485a2
shinn <zhouxy.vortex@gmail.com>
2025-04-24 21:49:48 +0800

    BST以及AVL树的相关笔记和代码

--- docs/struct/平衡AVL树.md
+++ docs/struct/平衡AVL树.md
@@ -29,6 +29,34 @@
 
 <img src="./images/avl_need_to_roate.png" style="zoom:33%;" />
 
+#### 树的高度
+树的高度是指一个节点到子树的最大路径。`Math.max(node.left.height, node.right.height) + 1` <++>
+
+```java
+// 获得树的高度
+private int height(Node node) {
+    return node == null ? -1 : node.height;
+}
+```
+
+更新树的高度（设置树的高度）:
+```java
+private void updateHeight(Node node) {
+    node.height = Math.max(height(node.left), height(node.right)) + 1;
+}
+```
+
+如果一个节点就是叶子节点，那么这个树的高度是`0` ，如果节点是`None` 则这个节点的高度为`-1` 。
+
+#### 平衡因子
+在上面的说明中，我们已经讲解了什么是*平衡因子* ，下面的代码展示了如何获得一个节点的平衡因子。
+
+```java
+private int balanceFactor(Node node) {
+    return height(node.left) - height(node.right);
+}
+```
+
 ### 如何去平衡二叉树
 
 总共有两种方式一种是`左旋` 另一种则是右旋`右旋` 。
@@ -45,6 +73,36 @@
 
 <img src="./images/avl_left_roate_rule_2.png" alt="左旋规则_旋转后" style="zoom:33%;" />
 
+**关键点** :
+1. **获得旋转节点的子节点**: `Node child = node.right` 也即这个节点的右孩。
+2. **获得右子节点的左节点**: `Node grandChild = child.left` 我们这里并不需要考虑到这个节点是否为空。因为就算是为空在旋转的时候也不会影响什么。
+3. **更新指针**: `child.left = node` 子节点的左孩子指向旋转的节点（表示旋转），新的子节点右孩子更新为原子节点的左孩子。
+
+实现在代码如下：
+
+```java
+private Node leftRotate(Node node) {
+    if (node == null) {
+        return null;
+    }
+
+    // 先找到需要旋转的节点的子节点
+    // 然后再找到这个节点的左节点
+    // 冲突的“左孩变右孩”
+    Node child = node.right;
+    Node grandChild = child.left;
+
+    child.left = node;
+    node.right = grandChild;
+
+    // 更新高度
+    updateHeight(node);
+    updateHeight(child);
+
+    return child;
+}
+```
+
 ### 右旋规则
 
 右旋即：**向右旋转，冲突的右孩变左孩**
@@ -53,6 +111,283 @@
 
 <img src="./images/avl_right_roate_rule_2.png" alt="右旋规则_旋转后" style="zoom:33%;" />
 
+这里的处理跟上面的左旋是类似的，需要注意更新是哪些节点指针。
+
+```java
+private Node rightRotate(Node node) {
+    if (node == null) {
+        return null;
+    }
+
+    Node child = node.left;
+    Node grandChild = child.right;
+
+    child.right = node;
+    node.left = grandChild;
+
+    updateHeight(node);
+    updateHeight(child);
+
+    return child;
+}
+```
+
+### 总共有几种情况
+
+#### LL型
+
+<img src="./images/avl_ll_type.png" alt="LL型" style="zoom:33%;" />
+
+如上图所示，假如现在我们插入`3` 这个节点，那么`14` 这个节点*平衡因子* 就为`2` 了，那么就是失衡的状态。而插入的这个节点呢又在这个失衡因子的左子树的左节点上，所以我们称这种情况也叫**LL** 型。
+
+平衡因子的角度：
+* 失衡结点: **平衡因子 = 2** 
+* 失衡结点左孩子: **平衡因子 = 1** 
+
+那么如何去处理这种*LL* 型呢，使得这棵树重新达到平衡的状态？
+
+<img src="./images/avl_ll_roate.png" alt="LL旋转处理重新达到平衡" style="zoom:33%;" />
+
+从上面的图可知，我们只需要对**失衡的结点做出右旋的操作** 即可。
+```java
+// 旋转，需要考虑的是4种情况LL RR LR RL
+private Node rotate(Node node) {
+    // 获取节点 node 的平衡因子
+    int balanceFactor = balanceFactor(node);
+    // 左偏树 L型
+    if (balanceFactor > 1) {
+        if (balanceFactor(node.left) >= 0) {
+            // LL型
+            // 右旋
+            return rightRotate(node);
+        } else {
+            // some code ....
+        }
+    }
+}
+```
+
+上面的代码展现了对于`LL` 型的处理。只需要对失衡的节点右旋即可。
+
+#### RR型
+
+<img src="./images/avl_rr_type.png" alt="RR型" style="zoom:33%;" />
+
+如上图所示，假如现在我们插入`17` 这个节点，那么`5` 这个节点*平衡因子* 就为`-2` 了，那么就是失衡状态。而插入的这个节点在失衡因子的右子树的右节点上，我们称这种情况为**RR** 型。
+
+平衡因子角度:
+* 失衡节点: **平衡因子= -2** 
+* 失衡节点的右孩子: **平衡因子= -1** 
+
+<img src="./images/avl_rr_roate.png" alt="RR型右旋处理重新达到平衡" style="zoom:33%;" />
+
+如上图所示，我们只需要对**失衡的节点进行右旋操作** 即可。
+
+```java
+// 旋转，需要考虑的是4种情况LL RR LR RL
+private Node rotate(Node node) {
+    // 获取节点 node 的平衡因子
+    int balanceFactor = balanceFactor(node);
+        if (balanceFactor < -1) {
+            if (balanceFactor(node.right) <= 0) {
+                // RR型
+                // 左旋
+                return leftRotate(node);
+            } else {
+                //some code
+            }
+        }
+    }
+}
+```
+
+上面的代码表示了，如何去处理RR型，我们只需要对失衡的节点左旋即可。
+
+#### LR型
+
+<img src="./images/avl_lr_type.png" alt="LR型" style="zoom:33%;" />
+
+如上图所示，当我们插入的节点`6` 时，`9` 这个节点失衡了，这个节点的平衡因子是`2` 而，其左节点的平衡因子为`-1` ，这个新插入的节点是在右子树里。我们称这种情况为**LR** 型。
+
+平衡因子角度:
+* 失衡节点: **平衡因子 = 2**
+* 失衡节点的左孩子: **平衡因子 = -1**
+
+<img src="./images/avl_lr_roate_step1.png" alt="先对左孩子进行左旋操作" style="zoom:33%;" />
+
+<img src="./images/avl_lr_roate_step2.png" alt="再对失衡节点进行右旋操作" style="zoom:33%;" />
+
+如上面两图所示:我们需要先对**失衡节点为的左孩子进行左旋操作，然后再对失衡节点进行右旋操作。**
+
+```java
+// 旋转，需要考虑的是4种情况LL RR LR RL
+private Node rotate(Node node) {
+    // 获取节点 node 的平衡因子
+    int balanceFactor = balanceFactor(node);
+    // 左偏树 L型
+    if (balanceFactor > 1) {
+        if (balanceFactor(node.left) >= 0) {
+            // LL型
+            // 右旋
+            return rightRotate(node);
+        } else {
+            // 重点关注
+            // LR型
+            // 先左旋后右旋
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+    }
+}
+```
+
+对于LR型，我们只需要对失衡的左节点先左旋，然后当失衡节点右旋即可。
+
+#### RL型
+
+<img src="./images/avl_rl_type.png" alt="RL型" style="zoom:33%;" />
+
+如上图所示，当我们插入的节点`8` 时，`5` 这个节点就失衡了，这个节点的平衡因子为`-2` ，而其右节点的平衡因子为`1` 时，这个新插入的节点在右子左子树里，我们称这种情况为**RL** 型。
+
+平衡因子角度:
+* 失衡节点: **平衡因子 = -2**
+* 失衡节点的右孩子: **平衡因子 = -1**
+
+<img src="./images/avl_rl_roate_step1.png" alt="RL型先右旋右孩子" style="zoom:33%;" />
+
+<img src="./images/avl_rl_roate_step2.png" alt="RL型失衡节点左旋" style="zoom:33%;" />
+
+```java
+// 旋转，需要考虑的是4种情况LL RR LR RL
+private Node rotate(Node node) {
+    // 获取节点 node 的平衡因子
+    int balanceFactor = balanceFactor(node);
+        if (balanceFactor < -1) {
+            if (balanceFactor(node.right) <= 0) {
+                // RR型
+                // 左旋
+                return leftRotate(node);
+            } else {
+                // RL型
+                // 先右旋后左旋
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+    }
+}
+```
+
+对于RL型，我们只需要对失衡节点的右子节点右旋，然后再对失衡节点左旋。
+
+#### 四种情况 LL、RR、LR、RL
+
+> [!important]
+> 表
+
+| 型 | 平衡因子情况                                                       | 操作                 |
+|----|--------------------------------------------------------------------|----------------------|
+| LL | 失衡节点: **平衡因子 = 2**<br/>失衡节点的左孩子: **平衡因子= 1**   | 右旋                 |
+| RR | 失衡节点: **平衡因子 = -2**<br/>失衡节点的右孩子: **平衡因子= -1** | 左旋                 |
+| LR | 失衡节点: **平衡因子 = 2**<br/>失衡节点的左孩子: **平衡因子= -1**  | 左旋左孩子, 然后右旋 |
+| RL | 失衡节点: **平衡因子 = -2**<br/>失衡节点的右孩子: **平衡因子= 1**  | 右旋右孩子, 然后左旋 |
+
+<img src="./images/avl_judge_type_diagram.png" alt="avl树判断流程" style="zoom:33%;" />
 
 
+### 插入
+我们在正常插入的时候如果遇到有多个节点平衡因子都不平衡的情况时。
 
+<img src="./images/avl_serval_nodes_need2roate.png" alt="AVL树可能有多个节点都需要进行调整" style="zoom:33%;" />
+
+如上图所示，这个棵树中插入了一个新的节点`9` ，这时候AVL树有多个节点都同出现了不平衡的情况,`3` 和`6` ，我们这时只需要调整距离9最近的失衡结点就可以了。
+
+```java
+
+public void insert(int val) {
+    this.root = insertHelper(root, val);
+}
+
+private Node insertHelper(Node node, int val) {
+    if (node == null) {
+        return new Node(val);
+    }
+
+    // 这里处理跟普通的BST是一样的，左节点<根节点，右节点>根节点
+    if (val > node.value) {
+        node.right = insertHelper(node.right, val);
+    } else if (val < node.value) {
+        node.left = insertHelper(node.left, val);
+    } else {
+        // 如果不需要更新则直接返回
+        return node;
+    }
+
+    // 实时更新节点高度
+    updateHeight(node);
+    // 旋转节点，旋转的方法里面对于各种情况已经做了判断，如果不需要处理的则不会有任何处理。
+    node = rotate(node);
+
+    return node;
+}
+```
+
+这里的重点是:递归里会返回新的旋转节点，这时候就会更新新的节点。`node.right = insertHelper(node.right, val)` 和`node.left = insertHelper(node.left, val)` 。
+
+### 删除
+当我们去删除AVL树中的节点时，他的操作与普通BST树是一样的，只不过当我们删除完了之后，还需要对父节点进行检查，查看是否有失衡的情况，有的话就进行调整。
+
+```java
+public void remove(int val) {
+    this.root = removeHelper(root, val);
+}
+
+private Node removeHelper(Node node, int val) {
+    if (node == null) {
+        return null;
+    }
+
+    if (node.value > val) {
+        node.left = removeHelper(node.left, val);
+    } else if (node.value < val) {
+        node.right = removeHelper(node.right, val);
+    } else {
+        // 叶子节点的数量 < 2
+        if (node.left == null || node.right == null) {
+            Node child = node.left != null ? node.left : node.right;
+
+            if (child == null) {
+                // 没有任何子树
+                return null;
+            } else {
+                // 更新节点为child
+                // 子节点 == 1 的时候，把原来的节点更新为最新的节点
+                node = child;
+            }
+        } else {
+            // 节点==2
+            // 现在这个右节点相当于新的root节点了
+            // 在右节点中使用中序去查找到最小值
+            Node temp = node.right;
+
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+
+            // 需要删除右侧的节点最小值
+            node.right = removeHelper(node.right, temp.value);
+            // 更新这个节点的值为找到值相当于替换
+            node.value = temp.value;
+        }
+    }
+
+    updateHeight(node);
+    node = rotate(node);
+    return node;
+}
+
+```
+
+上面的代码中，重点的代码在于，如果我们现在需要删除的这个节点有两个子节点，那么就需要去找到这个节点的右子树中最小的值，然后把最小值的节点也删除，也使用递归的形式去处理。因为因为这个节点删除之后，还可能造成此节点也失衡了，需要再进行旋转的操作。
+
+其他部分跟插件是类似的，也要遵守BST的规则。
--- struct/tree/BinarySearchTree.java
+++ struct/tree/BinarySearchTree.java
@@ -3,258 +3,190 @@ package tree;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * BST树
+ */
 public class BinarySearchTree {
-    // 数据
-    private int val;
-    // 左侧节点指针
-    private BinarySearchTree left;
-    // 右铡节点指针
-    private BinarySearchTree right;
 
-    /*-----------------------------构造方法---------------------------*/
-    public BinarySearchTree(int val) {
-        this.val = val;
-        left = right = null;
+    static class Node {
+        Integer value;
+        Node left;
+        Node right;
+
+        public Node(Integer value) {
+            this.value = value;
+        }
+
+        public Node(Integer value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Node() {
+        }
     }
 
-    public BinarySearchTree() {
-        left = right = null;
-    }
+    private Node root;
 
-    public BinarySearchTree(int val, BinarySearchTree left, BinarySearchTree right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+    public BinarySearchTree(Node root) {
+        this.root = root;
     }
 
     /**
-     * 以列表的第一个元素当作其根节点
+     * 插入切点
      *
-     * @param datas 需要构建BST的数据列表
-     * @return 当这个树构建完成之后，返回这个树的根节点对象
+     * @param value 需要插入值
      */
-    public static BinarySearchTree constructBST(List<Integer> datas) {
-        if (datas == null || datas.size() == 0) {
-            return null;
+    public void insert(int value) {
+        if (this.root == null) {
+            this.root = new Node(value);
+            return;
         }
 
-        // 把列表的第一个元素当作BST的根节点
-        BinarySearchTree root = new BinarySearchTree(datas.get(0));
+        Node cur = this.root, pre = null;
 
-        for (int i = 1; i < datas.size(); i++) {
-            int insertData = datas.get(i);
-            // 每次都从根节点进行查找的工作
-            BinarySearchTree temp = root;
-            while (temp != null) {
-                // 当前的节点数据与插入的数据相同的，则不会再插入了
-                if (temp.val == insertData) {
-                    break;
-                }
-                BinarySearchTree left = temp.left;
-                BinarySearchTree right = temp.right;
-                // 插入的数据是比现在这个节点小，则应该是左节点
-                if (temp.val > insertData) {
-                    if (left == null) {
-                        BinarySearchTree insertNode = new BinarySearchTree(insertData);
-                        temp.left = insertNode;
-                        break;
-                    } else {
-                        temp = left;
-                        continue;
-                    }
-                }
-
-                // 插入的数据是比当前的节点数据大，则应该是右节点
-                if (temp.val < insertData) {
-                    if (right == null) {
-                        BinarySearchTree insertNode = new BinarySearchTree(insertData);
-                        temp.right = insertNode;
-                        break;
-                    } else {
-                        temp = right;
-                    }
-                }
-            }
-        }
-
-        return root;
-    }
-
-    /**
-     * 向一个二叉搜索树中插入一个节点
-     *
-     * @param root    二叉搜索树的根节点
-     * @param element 需要插入的元素
-     */
-    @SuppressWarnings("unchecked")
-    public static void insert(BinarySearchTree root, int element) {
-
-        BinarySearchTree pre, cur;
-        cur = root;
-        pre = null;
+        // 这里对现在的这个树进行查找工作
         while (cur != null) {
-            if (cur.val == element) {
+            if (cur.value == value) {
                 return;
             }
 
             pre = cur;
-            if (cur.val < element) {
-                // 当前的节点值比插入值大则向右进行搜索
-                cur = cur.right;
-            } else {
-                // 当前的节点值比插入值小则向右进行搜索
+            if (cur.value < value) {
                 cur = cur.left;
-            }
-
-        }
-
-        // 向左或右插入节点
-        BinarySearchTree node = new BinarySearchTree(element);
-        if (pre.val < element) {
-            pre.right = node;
-        } else {
-            pre.left = node;
-        }
-
-    }
-
-    /**
-     * 删除二叉搜索树中的某个节点
-     *
-     * @param root 二叉搜索树
-     * @param cur  某个元素
-     */
-    public static void remove(BinarySearchTree root, int removeElement) {
-        BinarySearchTree pre, cur;
-
-        cur = pre = root;
-
-        // 去查找数据
-        while (cur != null && cur.val != removeElement) {
-            // 查找到需要删除的元素搜索
-            if (cur.val == removeElement) {
-                break;
-            }
-            pre = cur;
-
-            if (removeElement < cur.val) {
-                cur = cur.left;
-                continue;
-            }
-
-            if (removeElement > cur.val) {
+            } else if (cur.value > value) {
                 cur = cur.right;
             }
         }
 
-        // 现在开始分情况去处理
-        if (cur == null) {
-            // 树里面并没有这个元素
-            return;
-        }
-
-        BinarySearchTree replaceNode = null;
-        // 需要删除的这个节点度是0，即这个元素既没有左侧节点以及右侧的节点
-        if (cur.left == null && cur.right == null) {
-            // 左侧
-            replaceNode = null;
-        } else if (cur.left != null && cur.right != null) {
-            // 需要删除的元素度为2时
-            // 上节点pre也需要更新，当度为2时还需要按照中序遍历的形式去查找“右侧”subtree中的最小值
-
+        // 能跳出相应的循环表明现在已经找到了节点高度为0的目标位置
+        // 下面就是判断应该将新的节点插入到左节点上还是右节点上
+        if (pre.value < value) {
+            pre.right = new Node(value);
         } else {
-            // 度为1时的处理方式
-            if (cur.left != null) {
-                replaceNode = cur.left;
-            } else {
-                replaceNode = cur.right;
-            }
-        }
-        // 更新上节点
-        if (pre.left == cur) {
-            pre.left = replaceNode;
-        } else {
-            pre.right = replaceNode;
+            pre.left = new Node(value);
         }
     }
 
-    /**
-     * 搜索某个元素值
-     *
-     * @param root 二叉搜索树的根节点
-     * @param cur  搜索某个值
-     * @return 是否有这个元素，如果有的话则返回true，没有则返回false
-     */
-    public static BinarySearchTree search(BinarySearchTree root, int searchData) {
-        BinarySearchTree cur = root;
-
-        while (cur != null) {
-            if (cur.val == searchData) {
-                return cur;
-            }
-
-            if (cur.val > searchData) {
-                cur = cur.left;
-                continue;
-            }
-
-            cur = cur.right;
-        }
-
-        return cur;
-    }
-
-    /**
-     * 中序遍历
-     *
-     * 对于BST来讲，使用中序遍历完成之后得到的这个结果就是有序的结果列表了
-     * 相当于是进行了一次排序操作
-     *
-     * @param root  需要遍历的根节点
-     * @param lists 中序遍历完成后的结果列表
-     */
-    public static void inOrder(BinarySearchTree root, List<BinarySearchTree> lists) {
+    public void remove(int value) {
         if (root == null) {
             return;
         }
 
-        inOrder(root.left, lists);
-        lists.add(root);
-        inOrder(root.right, lists);
+        Node cur = this.root, pre = null;
+
+        while (cur != null) {
+            if (cur.value == value) {
+                break;
+            }
+
+            pre = cur;
+            if (cur.value < value) {
+                cur = cur.right;
+            } else {
+                cur = cur.left;
+            }
+        }
+
+        // 说明没有找到需要删除的节点
+        if (cur == null) {
+            return;
+        }
+        if (cur.left == null || cur.right == null) {
+            // 说明现在需要删除的这个节点有0个或1节点
+            Node child = cur.left != null ? cur.left : cur.right;
+            if (cur != root) {
+                // 判断cur在上一层节点的左子树还是右子树上然后再进行一次更新
+                if (pre.left == cur) {
+                    pre.left = child;
+                } else {
+                    pre.right = child;
+                }
+            } else {
+                // 如果现在删除的节点是根节则需要替换根节点为子节点
+                root = child;
+            }
+        } else {
+            // 这里是找到当前节点的右子树中最小的值然后替换这个节点
+            Node temp = cur.right;
+            // 逻辑相当于找到右子树，然后使用中序遍历的方式找到第一个值（即最小值的结点）
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+
+            // 这里再递归找到需要替换cur值的这个节点，把它给删除掉
+            remove(temp.value);
+            // 更新cur的值为右子树中最左的值
+            cur.value = temp.value;
+        }
     }
 
     /**
-     * 测试方法
+     * 判断数据是否存在，本质上也是一种查找的过程
      */
+    public boolean exist(Integer value) {
+        if (value == null) {
+            return false;
+        }
+
+        Node cur = root;
+        while (cur != null) {
+            if (cur.value == value) {
+                return true;
+            }
+
+            if (cur.value < value) {
+                cur = cur.right;
+            } else {
+                cur = cur.left;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * mid-order sequence
+     *
+     * @param node   起始的根节点数据
+     * @param values 按照mid-order顺序得到的列表
+     */
+    public void inOrder(Node node, List<Integer> values) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, values);
+        values.add(node.value);
+        inOrder(node.right, values);
+    }
+
+    public void preOrder(Node node, List<Integer> values) {
+        if (node == null) {
+            return;
+        }
+
+        values.add(node.value);
+        preOrder(node.left, values);
+        preOrder(node.right, values);
+    }
+
+    public void lastOrder(Node node, List<Integer> values) {
+        if (node == null) {
+            return;
+        }
+
+        lastOrder(node.left, values);
+        lastOrder(node.right, values);
+        values.add(node.value);
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
 
-        list.add(8);
-        list.add(6);
-        list.add(7);
-        list.add(9);
-        list.add(15);
-        list.add(10);
-        list.add(26);
-
-        BinarySearchTree root = constructBST(list);
-        List<BinarySearchTree> inOrderResultList = new ArrayList<>();
-        inOrder(root, inOrderResultList);
-
-        for (BinarySearchTree i : inOrderResultList) {
-            System.out.print(i.val + " ");
-        }
-        System.out.println();
-
-        insert(root, 13);
-        inOrderResultList = new ArrayList<>();
-        inOrder(root, inOrderResultList);
-        for (BinarySearchTree i : inOrderResultList) {
-            System.out.print(i.val + " ");
-        }
-        System.out.println();
-
-        BinarySearchTree searchNode = search(root, 2);
-        System.out.println(searchNode);
     }
 }
